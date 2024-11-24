1ea75315f743190bb69f12c68a7581aa4041d113
TuanSky4499 <dangquoctuan4499@gmail.com9>
2024-11-24 16:15:51 +0700

    24/11/2024: Initial Source

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("aa");
+  },
+};
+</script>
