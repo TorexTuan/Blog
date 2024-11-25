483a3b5b101779e49300c7f4dd93f38f72c720eb
TuanSky4499 <dangquoctuan4499@gmail.com9>
2024-11-25 07:34:43 +0700

    25/11/2024: Initial Source

--- app.vue
+++ app.vue
@@ -4,13 +4,3 @@
     <NuxtWelcome />
   </div>
 </template>
-
-<script>
-export default {
-  mounted() {
-    const a = new Map();
-    console.log(a);
-    console.log(a);
-  },
-};
-</script>
---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(
+  {
+    rules: {
+      "no-console": "error",
+    },
+  }
+  // Your custom configs here
+);
