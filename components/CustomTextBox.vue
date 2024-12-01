8aced22b3725ee6a2bcfdff5f548d9a90c5330d6
TuanSky4499 <dangquoctuan4499@gmail.com>
2024-12-01 22:27:12 +0700

    [01/12/2024]: Make input icon for login and register page

--- components/CustomTextBox.vue
+++ components/CustomTextBox.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="flex flex-col">
     <label v-if="!hideLabel" class="mb-1 text-sm">{{ label }}</label>
-    <input
-      color=""
-      :type="type"
-      name="username"
-      autocomplete="username"
-      class="bg-transparent border-white/50 px-4 py-2 placeholder:text-white placeholder:text-sm rounded-[40px] outline-none focus:border-white border-[1px]"
-      :placeholder="placeholder"
-    />
+    <div class="relative">
+      <input
+        name="username"
+        autocomplete="username"
+        :class="[
+          { 'pr-10': appendIconName || $slots['append-icon'] },
+          'bg-transparent w-full border-white/50 px-4 py-2 placeholder:text-white placeholder:text-sm rounded-[40px] outline-none focus:border-white border-[1px]'
+        ]"
+        :type="type"
+        :placeholder="placeholder"
+      />
+      <Icon
+        v-if="appendIconName"
+        class="absolute right-3 top-1/2 -translate-y-2/4"
+        :name="appendIconName"
+        :size="appendIconSize"
+      />
+      <div
+        v-if="$slots['append-icon']"
+        class="absolute right-3 top-1/2 -translate-y-2/4 flex items-center"
+      >
+        <slot name="append-icon" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -29,6 +45,14 @@ const { label, placeholder } = defineProps({
   type: {
     type: String,
     default: "text"
+  },
+  appendIconName: {
+    type: String,
+    default: ""
+  },
+  appendIconSize: {
+    type: String,
+    default: "24px"
   }
 });
 </script>
--- pages/auth.vue
+++ pages/auth.vue
@@ -3,31 +3,98 @@
     class="flex-center bg-[url('/assets/images/background-img.jpg')] h-screen"
   >
     <form
-      class="shadow-2xl shadow-black/50 rounded-xl p-10 flex-center flex-col border-[1px] border-white/20 backdrop-opacity-50 w-[400px]"
+      class="shadow-2xl shadow-black/50 rounded-xl p-10 flex-center flex-col border-[1px] border-white/20 backdrop-blur-xl w-[400px]"
     >
-      <p class="label mb-2">Login</p>
+      <p class="label mb-2">{{ isRegister ? "Register" : "Login" }}</p>
       <div class="flex flex-col w-full">
         <CustomTextBox
           hide-label
           class="my-3"
           label="Username"
           placeholder="Username"
+          append-icon-name="tabler:user-circle"
         />
         <CustomTextBox
           hide-label
           class="my-3"
           label="Password"
           placeholder="Password"
-          type="password"
-        />
-        <div class="my-2 flex justify-between">
+          :type="isShowPassword ? 'text' : 'password'"
+        >
+          <template #append-icon>
+            <Icon
+              v-if="isShowPassword"
+              name="tabler:eye"
+              class="cursor-pointer"
+              size="24px"
+              @click="isShowPassword = !isShowPassword"
+            />
+            <Icon
+              v-else
+              name="tabler:eye-closed"
+              class="cursor-pointer"
+              size="24px"
+              @click="isShowPassword = !isShowPassword"
+            />
+          </template>
+        </CustomTextBox>
+        <CustomTextBox
+          v-if="isRegister"
+          hide-label
+          class="my-3"
+          label="Confirm Password"
+          placeholder="Confirm Password"
+          :type="isShowConfirmPassword ? 'text' : 'password'"
+        >
+          <template #append-icon>
+            <Icon
+              v-if="isShowConfirmPassword"
+              name="tabler:eye"
+              class="cursor-pointer"
+              size="24px"
+              @click="isShowConfirmPassword = !isShowConfirmPassword"
+            />
+            <Icon
+              v-else
+              name="tabler:eye-closed"
+              class="cursor-pointer"
+              size="24px"
+              @click="isShowConfirmPassword = !isShowConfirmPassword"
+            />
+          </template>
+        </CustomTextBox>
+        <div v-if="!isRegister" class="my-2 flex justify-between">
           <CustomCheckbox type="text" name="Remember me" />
-          <span class="hover:underline text-sm cursor-pointer">
-            Forgot password?
-          </span>
+          <span class="underline-text"> Forgot password? </span>
         </div>
-        <CustomButton type="button" name="Login" class="mt-2 text-black" />
+        <CustomButton
+          type="button"
+          :name="isRegister ? 'Submit' : 'Login'"
+          class="mt-2 text-black"
+        />
+        <div class="text-center mt-3 flex-center">
+          <div v-if="isRegister">
+            <span class="text-sm"> You already had an account! </span>
+            <span class="underline-text" @click="isRegister = false">
+              Login
+            </span>
+          </div>
+          <div v-else>
+            <span class="text-sm"> Have you had an account? </span>
+            <span class="underline-text" @click="isRegister = true">
+              Register
+            </span>
+          </div>
+        </div>
       </div>
     </form>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isRegister = ref<boolean>(false);
+const isShowPassword = ref<boolean>(false);
+const isShowConfirmPassword = ref<boolean>(false);
+</script>
